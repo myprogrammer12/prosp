4e0c8868858f76d8db8fc48eef91c224344e40e6
myprogrammer12 <31843362+myprogrammer12@users.noreply.github.com>
2017-09-11 20:08:46 +0700

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,1 +1,4 @@
+
+<script style="text/javascript" src="https://cdn.rawgit.com/mowdot/mysitemap/master/sitemapku.js"></script>
+
 <script src="https://getsmarttvsamsung.blogspot.com/feeds/posts/default?max-results=9999&amp;alt=json-in-script&amp;callback=loadtoc"></script>
